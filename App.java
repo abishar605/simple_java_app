eaac8259c5a6887009e4851e13047a64c7d1627d
Nisha3113 <nisha.p2023@vitstudent.ac.in>
2026-03-24 15:27:42 +0530

    initial commit

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java CI/CD Application Started...");
+
+        while (true) {
+            Thread.sleep(5000); // wait for 5 seconds
+            System.out.println("Hello from Java CI/CD Pipeline!");
+        }
+    }
+}
